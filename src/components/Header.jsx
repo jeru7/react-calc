886f8ac2d863990a13d0dd29a7c9e082eab23917
jeru7 <john.ungab7@gmail.com>
2024-01-03 19:50:18 +0800

    components setup done

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/App.css";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <button className="darkToggler"></button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
