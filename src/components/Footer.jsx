a8b7b707e126d359ec85fdfbc3b4c58c36a6ccec
jeru7 <john.ungab7@gmail.com>
2024-01-04 10:17:29 +0800

    footer styles done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/App.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-icons">
+          <a href="https://github.com/jeru7" target="_blank" rel="noreferrer">
+            <i class="fa-brands fa-github icons"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jeru7/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-brands fa-linkedin-in icons"></i>
+          </a>
+        </div>
+        <div className="footer-name">
+          <p className="name">Developed by: jeru7</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
